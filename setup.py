5fd71dc7b06f2fe0ae6f5a66b1fdc501e188e318
jpg32 <j.pgarcia@me.com>
2020-08-10 18:57:46 +0200

    modifié :         MANIFEST.in 	modifié :         setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "1.0.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Discord and Tweeter plugin for OctoPrint"""
+plugin_description = """Twitter plugin for OctoPrint"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "jpg32"
